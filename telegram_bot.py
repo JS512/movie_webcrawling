4129c53b30dd1deeceecac149fdc358015574a62
ijs <js230023@gmail.com>
2024-01-03 21:46:25 +0900

    텔레그램봇 메시지 전송 테스트

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,11 +4,17 @@ import secret
 
 bot = telegram.Bot(token=secret.token)
 
+# async def getUpdate() :
+#     msg = await bot.getUpdates()
+#     for data in msg :
+#         print(data)
 
-async def getUpdate() :
-    msg = await bot.getUpdates()
-    for data in msg :
-        print(data)
+# asyncio.run(getUpdate())
+
+async def send() :
+    await bot.sendMessage(chat_id=secret.chat_id, text="테스트임")
 
 
-asyncio.run(getUpdate())
+event = asyncio.get_event_loop()
+event.run_until_complete(send())
+event.close()
