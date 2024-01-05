0b438e159178dc000e995816c73654b54d095eb8
ijs <js230023@gmail.com>
2024-01-05 19:27:49 +0900

    코드 정리 및 firefox 드라이버 사용

---
+++ movie_crawler_firefox.py
@@ -0,0 +1,87 @@
+url = 'http://www.cgv.co.kr/theaters/?areacode=01&theaterCode=0013&date=20240104'
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from bs4 import BeautifulSoup
+
+
+import telegram
+import asyncio
+import secret
+
+
+#비동기 함수인 텔레그램 챗 봇 사용시 결과는 나오지만 오류가 발생하여 이를 막기위함
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) 
+
+#텔레그램 봇이 메시지 전달하도록
+async def send(text, bot) :
+     #텔레그램 봇의 메시지 전달은 비동기..
+    await bot.sendMessage(chat_id=secret.chat_id, text=text)
+
+
+def job_function() :
+    #텔레그램 봇 인스턴스 생성
+    bot = telegram.Bot(token=secret.token)
+
+    #Firefox의 'headless' 옵션을 사용하기 위함.
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    # selenium의 webdriver를 통해 firefox 실행
+    # headless 옵션이 사용되었으므로, firefox 창이 뜨지 않음. headless 없으면 firefox 창이 뜸.
+    driver = webdriver.Firefox(options=options)
+
+    # 대상 웹 페이지로 이동.
+    driver.get(url)
+
+    # iframe의 XPath를 찾아서 해당 iframe으로 전환.
+    iframe_xpath = '//iframe[@id="ifrm_movie_time_table"]'  # 실제 웹 페이지의 iframe ID에 맞게 수정
+    # 페이지가 로드될 때까지 대기.
+    iframe = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.XPATH, iframe_xpath))
+    )
+    
+    # iframe 태그로 전환
+    driver.switch_to.frame(iframe)
+
+    # iframe 내용이 로드될 때까지 대기.
+    iframe_content_xpath = '//body'  # 실제 웹 페이지의 iframe 내용에 맞게 수정
+    iframe_content = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, iframe_content_xpath))
+    ).get_attribute("outerHTML")
+
+    # iframe에서 벗어나 원래의 상위 레벨로.
+    driver.switch_to.default_content()
+
+    # WebDriver를 종료.
+    driver.quit()
+
+    # 강의를 따라하다보니 bs4 모듈로 html 태그를 이용할 수 있도록.
+    soup = BeautifulSoup(iframe_content, 'html.parser')
+    # print(soup)
+
+    # imax가 존재하는 태그 리스트
+    is_imax_list = soup.select('span.imax')
+    imax_name_lst= []
+    if len(is_imax_list) > 0 :
+        for i in is_imax_list :
+            #imax가 존재하는 태그
+            imax = i.find_parent('div', class_='col-times')
+            #imax가 존재하는 영화의 이름
+            imax_name_lst.append(imax.select_one('div.info-movie > a > strong').text.strip())
+
+        # 챗봇으로 전송.
+        asyncio.run(send(str(imax_name_lst) + " IMAX가 열렸습니다." , bot))
+        # sched.pause()
+    else :
+        # 챗봇으로 전송.
+        asyncio.run(send("열린 IMAX가 없습니다." , bot))
+
+
+# sched = BlockingScheduler()
+# sched.add_job(job_function, 'interval', seconds=30)
+# sched.start()
+        
+job_function()
